cc1ec9f816b1de714b3a35dd210d040b58d90e77
Dominik.Kotecki <dominikus1910@gmail.com>
2020-04-12 14:53:19 +0000

    add system

---
+++ app/env/system.go
@@ -0,0 +1,34 @@
+package env
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+// GetEnvOrDefault gives env variable name or default value
+func GetEnvOrDefault(key, defaultValue string) string {
+	val := os.Getenv(key)
+	if val != "" {
+		return val
+	}
+	return defaultValue
+}
+
+// GetEnvOrDefault gives env name
+func GetEnvName() string {
+	env := GetEnvOrDefault("ENV", "PROD")
+	return strings.ToLower(env)
+}
+
+// FailOnError logs Fatal when erorr=
+func FailOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func GetNameWithEnvPrefix(name string) string {
+	return fmt.Sprintf("%s.%s", GetEnvName(), name)
+}
--- app/rabbitmq/rabbitmq.go
+++ app/rabbitmq/rabbitmq.go
@@ -1,10 +1,21 @@
 package rabbitmq
 
+import "github.com/streadway/amqp"
+
 type IRabbitMqClient interface {
-	Connect(connStr string)
+	Connect()
 	CreateChannel()
 	Close()
+	Subscribe(exchange, queue, topic string)
+	Publish(exchange, topic string)
 }
 
 type RabbitMqClient struct {
+	Connection *amqp.Connection
+}
+
+func NewRabbitMqClient(connStr string) (*RabbitMqClient, error) {
+	conn, err := amqp.Dial(connStr)
+	env.FailOnError(err, "Failed to connect to RabbitMQ")
+	return conn
 }
